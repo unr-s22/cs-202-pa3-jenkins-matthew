364813786af514e3d3eae6e4653af1dc45f0ec7c
Jenkins-Matthew <89361143+jenkins-matthew@users.noreply.github.com>
2022-03-17 15:52:18 -0700

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,10 +1,29 @@
 
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
+#include<string>
+#include<sstream>
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include "Money.h"
 
+using namespace std;
 
-class Accout {
-
+class Account {
+private:
+	float currentTotal = 0;
+	int amountDeposits = 0;
+	int amountWithDrawals = 0;
+	int num = 0;
+	string check[100];
+	stringstream account;
+	vector<Money *> money;
+public:
+	Account(Money *);
+	void makeDeposit(Money *);
+	void makeWithDrawal(Money *);
+	friend ostream& operator<<(ostream& os, const Account& account);
 };
 
 #endif //ACCOUNT_H
