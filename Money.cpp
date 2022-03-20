cd4a63ee02e5fa8198a190082faf4cb051c1993f
1102-Velasco-Jesamyn <89409203+1102-Velasco-Jesamyn@users.noreply.github.com>
2022-03-20 15:11:29 -0700

    Update Money.cpp

--- Money.cpp
+++ Money.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 #include "Money.h"
+
+
+Money::Money(const int &dollars, const int &cents):m_dollars(dollars), m_cents(cents) {
+
+}
+
+void Money::setMoney(int dollars, int cents) {
+    m_dollars = dollars;
+    m_cents = cents;
+}
+void Money::printMoney() {
+    std::cout << "$" << m_dollars << "." << m_cents << std::endl;
+}
