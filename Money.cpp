4d4e5d92182c6c138b1f53ea4b07e914250ef338
1102-Velasco-Jesamyn <89409203+1102-Velasco-Jesamyn@users.noreply.github.com>
2022-03-21 21:46:15 -0700

    Update Money.cpp

--- Money.cpp
+++ Money.cpp
@@ -2,15 +2,5 @@
 #include "Money.h"
 
 
-Money::Money(const int &dollars, const int &cents):m_dollars(dollars), m_cents(cents) {
-
-}
-
-void Money::setMoney(int dollars, int cents) {
-    m_dollars = dollars;
-    m_cents = cents;
-}
-ostream& operator << (ostream& os, const Money& money) {
-    os<< "$" << m_dollars << "." << m_cents << std::endl;
-    return os;
-}
+//incude account class
+//Money * m
